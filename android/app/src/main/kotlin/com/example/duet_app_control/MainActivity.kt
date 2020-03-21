848e97000ce4399d6ee351970033c1ed9e93d9a5
Dennis Rosa <dennisrosa@myPrivateHotSpot.local>
2020-03-20 22:23:57 -0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/duet_app_control/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.duet_app_control
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
